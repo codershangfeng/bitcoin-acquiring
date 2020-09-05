4d81a064597dd159cff1a3df1fc98ee2995bf0b5
codershangfeng <codershangfeng@gmail.com>
2020-09-05 09:29:03 +0800

    Init

---
+++ src/main/kotlin/me/codershangfeng/bitcoinaquiring/BitcoinAcquiringApplication.kt
@@ -0,0 +1,11 @@
+package me.codershangfeng.bitcoinaquiring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BitcoinAcquiringApplication
+
+fun main(args: Array<String>) {
+	runApplication<BitcoinAcquiringApplication>(*args)
+}
---
+++ src/test/kotlin/me/codershangfeng/bitcoinaquiring/BitcoinAcquiringApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.codershangfeng.bitcoinaquiring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BitcoinAcquiringApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
